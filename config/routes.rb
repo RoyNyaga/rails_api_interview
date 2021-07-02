7d7028e8133cb6d933aa2671c1fbead722a02de6
RoyNyaga <nyagaandreroy@gmail.com>
2021-07-03 00:59:17 +0100

    create post scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :users
   namespace :api do 
     namespace :v1 do 
       # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
       post 'authenticate', to: 'authentication#authenticate'
       resources :users
+      resources :posts
     end 
   end
 end
