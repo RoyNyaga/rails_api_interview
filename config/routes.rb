adfcb63f53c8d9ec90f404d0fed7c1cbae7bdb75
RoyNyaga <nyagaandreroy@gmail.com>
2021-07-02 22:51:01 +0100

    name space request to api:v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  post 'authenticate', to: 'authentication#authenticate'
+  namespace :api do 
+    namespace :v1 do 
+      # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+      post 'authenticate', to: 'authentication#authenticate'
+    end 
+  end
 end
