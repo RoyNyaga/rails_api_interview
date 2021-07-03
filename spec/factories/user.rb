a28fc2bb990be8237a877bff08539e637ea4981a
RoyNyaga <nyagaandreroy@gmail.com>
2021-07-03 12:35:30 +0100

    create user factory

---
+++ spec/factories/user.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :user do
+    email { Faker::Internet.email }
+    name { Faker::Name.name } # TODO: required???
+    password: { Faker::Internet.password(min_length: 8) }
+  end
+end
