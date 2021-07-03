d612f6481c1299d27ecef57a857911ee98a8f3b4
RoyNyaga <nyagaandreroy@gmail.com>
2021-07-03 12:14:46 +0100

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   has_secure_password
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  validates :email, uniqueness: true, presence: true, 
+  format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/,
+  message: "please enter a correct email format (ex: youname@gmail.com" }
+  validates :password, length: { minimum: 5 }
 end
